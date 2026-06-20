089fb36c39ee09e540da3f4afea0b6a350719b01
Beatriz Soares <beatrizsdcosta@hotmail.com>
2026-06-20 17:08:27 -0300

    main

---
+++ UNIDADE 3/Aula 078 c.c
@@ -0,0 +1,15 @@
+#include "combate.h"
+
+int calcular_ataque_total(int ataque_base, int bonus_arma) {
+    return ataque_base + bonus_arma;
+}
+
+int aplicar_dano(int vida_atual, int dano_sofrido) {
+    int vida_restante = vida_atual - dano_sofrido;
+    
+    if (vida_restante < 0) {
+        return 0;
+    }
+    
+    return vida_restante;
+}
